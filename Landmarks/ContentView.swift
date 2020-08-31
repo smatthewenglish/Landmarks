71e6d42a47a4aadcd1e5871a01233ed315973e13
smatthewenglish <s.matthew.english@gmail.com>
2020-08-30 20:07:42 -0400

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by S. Matthew English on 8/30/20.
+//  Copyright © 2020 S. Matthew English. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
